798abb3ddedbba2ee49f0c36f6b2cde409ea6fec
Lakhan-Bhutiya <bhutiyalakhan13@gmail.com>
2026-01-30 15:54:57 +0530

    feat: implement moderation worker and secure API keys

--- chat-ai/apps/moderation-worker/src/groq.client.js
+++ chat-ai/apps/moderation-worker/src/groq.client.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.groq = void 0;
 var groq_sdk_1 = require("groq-sdk");
 exports.groq = new groq_sdk_1.default({
-    apiKey: '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', // 🔴 hard-coded as requested
+     // 🔴 hard-coded as requested
 });
